a7d95a53a950de26e2b12384f8020f5e4b26864d
vaisak1992 <133529707+vaisak1992@users.noreply.github.com>
2026-03-23 09:01:00 +0530

    Update request.js
    updated localhost with the server's IP

--- front/src/Rqst/request.js
+++ front/src/Rqst/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const Base_Url="http://localhost:5000/api/"
+const Base_Url="http://13.232.145.41:5000/api/"
 
 export const publicRequest=axios.create({baseURL:Base_Url})
