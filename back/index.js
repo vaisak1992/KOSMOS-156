5083f6aee6054c4866d9ad90fb362533762ed299
vaisak1992 <133529707+vaisak1992@users.noreply.github.com>
2026-03-23 11:57:03 +0530

    Update index.js
    updated index.js file with new CORS

--- back/index.js
+++ back/index.js
@@ -3,13 +3,15 @@ const app=express()
 const mongoose=require('mongoose')
 const dotenv=require('dotenv')
 const cors=require('cors')
-
 dotenv.config()
-app.use(cors())
+
+app.use(cors({
+    origin: ["https://www.test-project.xyz", "https://test-project.xyz"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
 
 const route=require('./router/auth')
-
-
 mongoose.connect(process.env.Mongodb_Url).then((data)=>{
     console.log('database connected');
 }).catch((err)=>{
@@ -17,7 +19,6 @@ mongoose.connect(process.env.Mongodb_Url).then((data)=>{
 })
 app.use(express.json())
 app.use('/api/Cli',route)
-
 app.listen(5000,()=>{
     console.log("port 5000 connected");
 })
