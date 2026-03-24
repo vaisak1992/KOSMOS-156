ab9c7fd12f08bd5d01c4cb2e9ac95f2b00e67f25
vaisak1992 <133529707+vaisak1992@users.noreply.github.com>
2026-03-24 09:34:43 +0530

    Update main.tf
    updated main.tf

--- main.tf
+++ main.tf
@@ -26,12 +26,59 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 }
 
-# Block public access (security best practice)
+# Ownership controls (recommended)
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# Disable public access block (to allow website access)
 resource "aws_s3_bucket_public_access_block" "block_public" {
   bucket = aws_s3_bucket.bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Enable static website hosting
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+# Bucket policy to allow public read
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.bucket.id
+
+  depends_on = [aws_s3_bucket_public_access_block.block_public]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+# Output website URL
+output "website_url" {
+  value = "http://${aws_s3_bucket.bucket.bucket}.s3-website-ap-south-1.amazonaws.com"
 }
